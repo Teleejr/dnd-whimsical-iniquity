f1521d75fb73846b81c10015404704ad1ac50038
Teleejr <81259920+Teleejr@users.noreply.github.com>
2024-08-31 16:31:42 -0400

    Reorganized file structure

---
+++ iniquity/src/main/java/com/whimsical/iniquity/dnd/WhimsicalIniquityApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.whimsical.iniquity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhimsicalIniquityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhimsicalIniquityApplication.class, args);
+	}
+
+}
---
+++ iniquity/src/test/java/com/dnd/whimsical/iniquity/WhimsicalIniquityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.whimsical.iniquity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhimsicalIniquityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
